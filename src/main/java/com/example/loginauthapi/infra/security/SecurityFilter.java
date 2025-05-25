e69eb46ed7ec7073b43eaa103b6a90c37f0bf798
Kenzo Takeda <gkenzot@gmail.com>
2025-05-25 12:05:56 -0300

    notnull notation

--- src/main/java/com/example/loginauthapi/infra/security/SecurityFilter.java
+++ src/main/java/com/example/loginauthapi/infra/security/SecurityFilter.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 
@@ -25,9 +26,9 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-        HttpServletRequest request,
-        HttpServletResponse response,
-        FilterChain filterChain
+        @NonNull HttpServletRequest request,
+        @NonNull HttpServletResponse response,
+        @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         var token = recoverToken(request);
         
